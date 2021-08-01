74be50bf86625b84e96cc9386cf0cb72c5f7cd49
bhufnagel <Brodinator@Live.com>
2021-08-01 15:35:09 -0500

    Generated Backend

---
+++ Backend/src/main/java/com/TheBrodieBunch/BoozeCtrl/BoozeCtrlApplication.java
@@ -0,0 +1,13 @@
+package com.TheBrodieBunch.BoozeCtrl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoozeCtrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoozeCtrlApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/TheBrodieBunch/BoozeCtrl/BoozeCtrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TheBrodieBunch.BoozeCtrl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoozeCtrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
